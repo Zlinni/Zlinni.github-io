e7ae466367af44dd185a347f0ed1e5e79dbf56ec
Zlinni <984328216@qq.com>
2021-12-11 20:23:57 +0800

    Site updated: 2021-12-11 20:23:56

---
+++ js/eve.js
@@ -0,0 +1,9 @@
+var fancybox = document.getElementsByClassName('fancybox');
+var beforeimg = document.getElementById('beforeimg');
+beforeimg.setAttribute("data-lazy-src","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/beforenew.png");
+var afterimg = document.getElementById('afterimg');
+afterimg.setAttribute("data-lazy-src","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/afternew.png");
+console.log(beforeimg);
+//fancybox[0].setAttribute("href","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/beforenew.png");
+//beforeimg.src='https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/beforenew.png';
+//fancybox[1].setAttribute("href","https://cdn.jsdelivr.net/gh/Zlinni/Pic/img/afternew.png");
