953778bbba5be4d55ce422f47060e941449ae932
Zlinni <984328216@qq.com>
2021-12-12 20:03:20 +0800

    Site updated: 2021-12-12 20:03:19

--- js/addele.js
+++ js/addele.js
@@ -3,9 +3,15 @@ console.log(window.location);
 var newheadBox = document.createElement('div');
 newheadBox.classList.add('newheadBox');
 if (window.location.pathname.match('post')) {
-    console.log('wenzhangye');
-    iscenterBox[10].classList.add('centerBox');
-    iscenterBox[10].insertBefore(newheadBox, iscenterBox[10].childNodes[0]);
+    try {
+        console.log('wenzhangye');
+        iscenterBox[10].classList.add('centerBox');
+        iscenterBox[10].insertBefore(newheadBox, iscenterBox[10].childNodes[0]);
+    } catch (error) {
+        iscenterBox[4].classList.add('centerBox');
+        console.log('qiteye');
+        iscenterBox[4].insertBefore(newheadBox, iscenterBox[4].childNodes[0]);
+    }
 } else {
     iscenterBox[4].classList.add('centerBox');
     console.log('qiteye');
