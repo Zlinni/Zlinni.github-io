299cf771751b116034e7460ae68a5ef5cca1b7ed
Zlinni <984328216@qq.com>
2021-12-12 13:50:44 +0800

    Site updated: 2021-12-12 13:50:43

--- js/addele.js
+++ js/addele.js
@@ -1,8 +1,13 @@
 var iscenterBox = document.getElementsByClassName('is-center');
 console.log(window.location);
-iscenterBox[4].classList.add('centerBox');
-iscenterBox[10].classList.add('centerBox');
 var newheadBox = document.createElement('div');
 newheadBox.classList.add('newheadBox');
-iscenterBox[4].insertBefore(newheadBox, iscenterBox[4].childNodes[0]);
-iscenterBox[10].insertBefore(newheadBox, iscenterBox[10].childNodes[0]);
+if (window.location.pathname.match('post')) {
+    console.log('wenzhangye');
+    iscenterBox[10].classList.add('centerBox');
+    iscenterBox[10].insertBefore(newheadBox, iscenterBox[10].childNodes[0]);
+} else {
+    iscenterBox[4].classList.add('centerBox');
+    console.log('qiteye');
+    iscenterBox[4].insertBefore(newheadBox, iscenterBox[4].childNodes[0]);
+}
