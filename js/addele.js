ef94e8a757b322c76ff07fcfde7ba5466bc5cf62
Zlinni <984328216@qq.com>
2021-12-19 12:14:38 +0800

    Site updated: 2021-12-19 12:14:36

--- js/addele.js
+++ js/addele.js
@@ -1,19 +1,6 @@
-var iscenterBox = document.getElementsByClassName('is-center');
-console.log(window.location);
+var cardinfoBox = document.getElementsByClassName('card-info')[0].children[0];
+console.log(cardinfoBox);
 var newheadBox = document.createElement('div');
 newheadBox.classList.add('newheadBox');
-if (window.location.pathname.match('post')) {
-    try {
-        console.log('wenzhangye');
-        iscenterBox[10].classList.add('centerBox');
-        iscenterBox[10].insertBefore(newheadBox, iscenterBox[10].childNodes[0]);
-    } catch (error) {
-        iscenterBox[4].classList.add('centerBox');
-        console.log('qiteye');
-        iscenterBox[4].insertBefore(newheadBox, iscenterBox[4].childNodes[0]);
-    }
-} else {
-    iscenterBox[4].classList.add('centerBox');
-    console.log('qiteye');
-    iscenterBox[4].insertBefore(newheadBox, iscenterBox[4].childNodes[0]);
-}
+cardinfoBox.classList.add('centerBox');
+cardinfoBox.insertBefore(newheadBox,cardinfoBox.childNodes[0]);
